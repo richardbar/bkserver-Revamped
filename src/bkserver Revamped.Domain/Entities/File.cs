27e2d33e4a9cdc93753a88d889ec8467f826e4e5
Rikarnto Bariampa <richard1996ba@gmail.com>
2022-08-08 18:26:56 +0300

    Added File Entity

---
+++ src/bkserver Revamped.Domain/Entities/File.cs
@@ -0,0 +1,41 @@
+﻿using bkserver.Domain.Entities.Common;
+
+namespace bkserver.Domain.Entities;
+
+public class File
+{
+    public FileId Id { get; init; }
+
+    public FileName Name { get; init; }
+
+    public FileSize Size { get; init; }
+
+    public FileLastModified LastModified { get; init; }
+
+    public File(FileInfo fileInfo)
+    {
+        ArgumentNullException.ThrowIfNull(fileInfo);
+
+        if (!fileInfo.Exists)
+            throw new ArgumentException("File does not exists", nameof(fileInfo));
+
+        Id = FileId.From(Guid.NewGuid());
+        Name = FileName.From(fileInfo.Name);
+        Size = FileSize.From(fileInfo.Length);
+        LastModified = FileLastModified.From(fileInfo.LastWriteTimeUtc);
+    }
+
+    public File(string fileName)
+    {
+        ArgumentNullException.ThrowIfNull(fileName);
+
+        var fileInfo = new FileInfo(fileName);
+        if (!fileInfo.Exists)
+            throw new ArgumentException("File does not exists", nameof(fileName));
+
+        Id = FileId.From(Guid.NewGuid());
+        Name = FileName.From(fileInfo.Name);
+        Size = FileSize.From(fileInfo.Length);
+        LastModified = FileLastModified.From(fileInfo.LastWriteTimeUtc);
+    }
+}
