eb3b2974c1b307d10b003d0af28bc185519f365e
Rikarnto Bariampa <richard1996ba@gmail.com>
2022-08-08 18:26:38 +0300

    Added Common FileSize class

---
+++ src/bkserver Revamped.Domain/Entities/Common/FileSize.cs
@@ -0,0 +1,12 @@
+﻿using ValueOf;
+
+namespace bkserver.Domain.Entities.Common;
+
+public sealed class FileSize : ValueOf<long, FileSize>
+{
+    protected override void Validate()
+    {
+        if (Value < 0)
+            throw new ArgumentException("File size cannot be negative", nameof(FileSize));
+    }
+}
