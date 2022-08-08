669ef1b1b8293cdfffa6903d35cc8507470a66ff
Rikarnto Bariampa <richard1996ba@gmail.com>
2022-08-08 18:22:42 +0300

    Added Common FileId class

---
+++ src/bkserver Revamped.Domain/Entities/Common/FileId.cs
@@ -0,0 +1,12 @@
+﻿using ValueOf;
+
+namespace bkserver.Domain.Entities.Common;
+
+public sealed class FileId : ValueOf<Guid, FileId>
+{
+    protected override void Validate()
+    {
+        if (Value == Guid.Empty)
+            throw new ArgumentException("File Id cannot be empty", nameof(FileId));
+    }
+}
