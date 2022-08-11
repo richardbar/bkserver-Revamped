18356f2b634c2e042a47e73e6b109331cabef616
Rikarnto Bariampa <richard1996ba@gmail.com>
2022-08-11 12:43:12 +0300

    Added FolderContent Entity

---
+++ src/bkserver Revamped.Domain/Entities/FolderContent.cs
@@ -0,0 +1,12 @@
+﻿using bkserver.Domain.Entities.Common;
+
+namespace bkserver.Domain.Entities;
+
+public class FolderContent
+{
+    public FolderContentId Id { get; init; }
+
+    public IEnumerable<File> Files { get; init; }
+
+    public IEnumerable<Folder> Folders { get; init; }
+}
