148fe9a1b72c88c849d50ab254c6861a44507081
Rikarnto Bariampa <richard1996ba@gmail.com>
2022-08-11 12:43:01 +0300

    Added Common FolderContentId class

---
+++ src/bkserver Revamped.Domain/Entities/Common/FolderContentId.cs
@@ -0,0 +1,12 @@
+﻿using ValueOf;
+
+namespace bkserver.Domain.Entities.Common;
+
+public class FolderContentId : ValueOf<Guid, FolderContentId>
+{
+    protected override void Validate()
+    {
+        if (Value == Guid.Empty)
+            throw new ArgumentException("Folder Content Id cannot be empty", nameof(FileId));
+    }
+}
