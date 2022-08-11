9a59d72c08c7653b3a59f5459d646416328bcbce
Rikarnto Bariampa <richard1996ba@gmail.com>
2022-08-11 12:55:17 +0300

    Added DirectoryInfoExtension

---
+++ src/bkserver Revamped.Domain/Extensions/System/IO/DirectoryInfoExtension.cs
@@ -0,0 +1,31 @@
+﻿using bkserver.Domain.Entities;
+
+using File = bkserver.Domain.Entities.File;
+
+namespace bkserver.Domain.Extensions.System.IO;
+
+public static class DirectoryInfoExtension
+{
+    public static FolderContent GetFolderContent(this DirectoryInfo directoryInfo)
+    {
+        var filesList = new List<File>();
+        var foldersList = new List<Folder>();
+
+        FileInfo[] files = directoryInfo.GetFiles();
+        DirectoryInfo[] directories = directoryInfo.GetDirectories();
+
+        foreach (FileInfo file in files)
+            filesList.Add(new File(file));
+        
+        foreach (DirectoryInfo directory in directories)
+            foldersList.Add(new Folder(directory));
+
+        var folderContent = new FolderContent()
+        {
+            Files = filesList,
+            Folders = foldersList
+        };
+
+        return folderContent;
+    }
+}
