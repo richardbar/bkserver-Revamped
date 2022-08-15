1049f900d5d475cfd925b844d3360fc62c9c1728
Rikarnto Bariampa <richard1996ba@gmail.com>
2022-08-15 17:07:43 +0300

    Added Human readable file size extension to common FileSize class

---
+++ src/bkserver Revamped.Domain/Extensions/bkserver/Domain/Entities/Common/FileSizeExtension.cs
@@ -0,0 +1,23 @@
+﻿using bkserver.Domain.Entities.Common;
+
+namespace bkserver.Domain.Extensions.bkserver.Domain.Entities.Common;
+
+public static class FileSizeExtension
+{
+    private static string[] Sizes { get; } = { "B", "KB", "MB", "GB", "TB", "PB", "EB" };
+    
+    public static string GetHumanReadableFileSize(this FileSize fileSize)
+    {
+        double len = fileSize.Value;
+        int order = 0;
+        const int comparisonValue = (int)(1.01 * 1024);
+
+        while (comparisonValue <= len)
+        {
+            order++;
+            len /= 1024;
+        }
+
+        return String.Format("{0:0.##} {1}", len, Sizes[order]);
+    }
+}
