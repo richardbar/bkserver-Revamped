ef30942d924c47d18d1fa35de17eb1621c1f140c
Rikarnto Bariampa <richard1996ba@gmail.com>
2022-08-15 17:08:52 +0300

    Added IndexRequest for browsing files

---
+++ src/bkserver Revamped.Domain/Contracts/Requests/Browse/IndexRequest.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bkserver.Domain.Contracts.Requests.Browse;
+
+public sealed class IndexRequest
+{
+    [FromQuery]
+    public string Path { get; init; } = default!;
+}
