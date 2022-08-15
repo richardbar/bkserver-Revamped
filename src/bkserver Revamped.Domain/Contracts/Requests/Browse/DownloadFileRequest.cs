223468263ca3afa4bbcf53ddcc6f1a9f5c659351
Rikarnto Bariampa <richard1996ba@gmail.com>
2022-08-15 17:32:06 +0300

    Added DownloadFileRequest for downloading files

---
+++ src/bkserver Revamped.Domain/Contracts/Requests/Browse/DownloadFileRequest.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bkserver.Domain.Contracts.Requests.Browse;
+
+public sealed class DownloadFileRequest
+{
+    [FromQuery]
+    public string Directory { get; init; } = default!;
+}
